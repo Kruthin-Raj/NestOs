6be6cc16f164bd484d3b8d8118bbeff8324bff67
jathinreddy-5 <jathinreddy024@gmail.com>
2026-08-25 21:57:17 +0530

    chore: remove test file

--- apps/api/test-resolve.js
+++
@@ -1,6 +0,0 @@
-async function test() {
-  const res = await fetch('https://maps.app.goo.gl/dBny6HSUssx7r3rG8', { redirect: 'manual' });
-  console.log(res.status);
-  console.log(res.headers.get('location'));
-}
-test();
